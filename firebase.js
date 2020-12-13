1f1c951f5f834c0212cc51df56235948d4c8efe9
Rancha/124 <2016eeb1089@iitrpr.ac.in>
2020-12-13 09:17:49 +0530

    css card

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyDILmKEbRBCpRW1XvtqKpEib3ELlB1leqg",
+  authDomain: "css-design-22521.firebaseapp.com",
+  projectId: "css-design-22521",
+  storageBucket: "css-design-22521.appspot.com",
+  messagingSenderId: "180470296007",
+  appId: "1:180470296007:web:da256abb3cf456dbf2fde2",
+  measurementId: "G-YEZFPE1GEK",
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="product">
+        <div class="product__photo">
+          <img
+            src="https://i.pinimg.com/originals/87/33/59/8733594e8813886a0587bb3366123236.jpg"
+            alt="img still loading"
+          />
+        </div>
+        <div className="product__info">
+          <div className="title">
+            <h1>Great Nature</h1>
+            <span>COD: 999</span>
+          </div>
+          <div className="price">
+            R$ <span>1.23</span>
+          </div>
+          <div className="variant">
+            <h3>SELECT A COLOR</h3>
+            <ul className="align__images">
+              <li>
+                <img
+                  src="https://res.cloudinary.com/john-mantas/image/upload/v1537302064/codepen/delicious-apples/green-apple2.png"
+                  alt="green apple"
+                />
+              </li>
+              <li>
+                <img
+                  src="https://res.cloudinary.com/john-mantas/image/upload/v1537302752/codepen/delicious-apples/yellow-apple.png"
+                  alt="yellow apple"
+                />
+              </li>
+              <li>
+                <img
+                  src="https://res.cloudinary.com/john-mantas/image/upload/v1537302427/codepen/delicious-apples/orange-apple.png"
+                  alt="orange apple"
+                />
+              </li>
+              <li>
+                <img
+                  src="https://res.cloudinary.com/john-mantas/image/upload/v1537302285/codepen/delicious-apples/red-apple.png"
+                  alt="red apple"
+                />
+              </li>
+            </ul>
+          </div>
+          <div className="description">
+            <h3>BENEFITS</h3>
+            <ul>
+              <li>Good for health</li>
+              <li>May be good for Instagram posts</li>
+              <li>May be good for mental health</li>
+              <li>They're linked to a lowest risk of heart disease</li>
+            </ul>
+          </div>
+          <button className="add__cart">ADD TO CART</button>
+        </div>
+      </div>
     </div>
   );
 }
